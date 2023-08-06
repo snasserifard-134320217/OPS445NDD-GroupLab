2eff5213818f461b2fb40aea5c9bfe314065fd55
snasserifard-134320217 <133617905+snasserifard-134320217@users.noreply.github.com>
2023-08-05 22:31:41 -0400

    Updating rock_paper_scissors (adding loop and exit statement).py

--- Group Lab/rock_paper_scissors.py
+++ Group Lab/rock_paper_scissors.py
@@ -2,17 +2,24 @@ import random
 
 options = ["rock", "paper", "scissors"]  # list of options for the computer
 
-print("Welcome to Rock, Paper, Scissors!")
-user_option = input("Enter your choice (rock, paper, scissors): ")
-computer_option = random.choice(options)  # This will choose a random option from your list for the computer
+play_again = True
 
-print(f"You chose {user_option}, computer chose {computer_option}.")
+while play_again:
+    print("Welcome to Rock, Paper, Scissors!")
+    user_option = input("Enter your choice (rock, paper, scissors): ")
+    computer_option = random.choice(options)  # This will choose a random option from your list for the computer
 
-if user_option == computer_option:
-    print("It's a tie!")
-elif (user_option == "rock" and computer_option == "scissors") or \
-     (user_option == "scissors" and computer_option == "paper") or \
-     (user_option == "paper" and computer_option == "rock"):
-    print("You win!")
-else:
-    print("You lose!")
+    print(f"You chose {user_option}, computer chose {computer_option}.")
+
+    if user_option == computer_option:
+        print("It's a tie!")
+    elif (user_option == "rock" and computer_option == "scissors") or \
+         (user_option == "scissors" and computer_option == "paper") or \
+         (user_option == "paper" and computer_option == "rock"):
+        print("You win!")
+    else:
+        print("You lose!")
+
+    play_again_input = input("Do you want to play again? (yes/no): ")
+    if play_again_input.lower() != "yes":
+        play_again = False
