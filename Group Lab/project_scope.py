8a4a53166318c9b9794fb4c706374ce6558cf9ff
snasserifard-134320217 <133617905+snasserifard-134320217@users.noreply.github.com>
2023-08-05 22:00:02 -0400

    Create project_scope.py

---
+++ Group Lab/project_scope.py
@@ -0,0 +1,5 @@
+"""
+Project Scope:
+
+This project aims to develop a simple, interactive Rock, Paper, Scissors game. The game will be developed in Python and will allow a user to play against the computer. The goal is to follow the Agile Software Development Life Cycle (SDLC) to ensure a quality game and timely delivery. The project will involve planning, designing, developing, testing, deploying, and maintaining the game.
+"""
